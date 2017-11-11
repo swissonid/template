ee1c83d1c2a9d6c31f8ab049a4dd45be38521104
Patrice Mueller <mueller.patrice@gmail.com>
2017-11-11 13:47:18 +0100

    Updated ANROID_ARCH_VERSION to 1

--- app/src/main/java/ch/swissonid/template/di/AppModule.java
+++ app/src/main/java/ch/swissonid/template/di/AppModule.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import ch.swissonid.template.App;
 import dagger.Binds;
 import dagger.Module;
-import dagger.Provides;
 
 /**
  * Created by pmueller on 30.10.17.
@@ -18,8 +17,7 @@ public abstract class AppModule {
     @Binds
     abstract Application application(App app);
 
-    @Provides
-    static Context providesContext(Application application) {
-        return application;
-    }
+    @Binds
+    abstract Context providesContext(Application application);
+
 }
